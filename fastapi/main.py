4b69f3056140d1d0d045a6670ea1d275163f771d
pabloap7 <cieguitomaleta2005@gmail.com>
2025-01-15 10:23:42 +0100

    limpieza

--- fastapi/main.py
+++ fastapi/main.py
@@ -23,18 +23,8 @@ def read_root():
 
 @app.get("/test", response_class=HTMLResponse)
 async def test(request: Request):
-    
-   
     return templates.TemplateResponse(
-        request=request, name="index.html", context={"nombre": "pepe"}                                                      
-    )
-
-@app.get("/items", response_class=HTMLResponse)
-async def read_item(request: Request):
-    
-    students = [ {"nombre": "pepe", "edad": 20,"score": 50}, {"nombre": "pepe", "edad": 20,"score": 50}, {"nombre": "pepe", "edad": 20,"score": 90}]
-    return templates.TemplateResponse(
-        request=request, name="students.html", context={"nombre": "pepe","students": students}                                                      
+        request=request, name="index.html"                                                    
     )
 
 
